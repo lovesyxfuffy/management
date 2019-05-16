0aae00703068130a01d8d0b57691235ad300d8c2
yujingyang03 <yujingyang03@meituan.com>
2019-05-16 13:13:04 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,7 @@ export const constantRouterMap = [
   {
     path: publicPath+'/',
     component: Layout,
-    redirect: publicPath + '/home',
+    redirect: publicPath + '/login',
     hidden: true
   },
   {
@@ -285,7 +285,7 @@ export const constantRouterMap = [
 ]
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
 })
